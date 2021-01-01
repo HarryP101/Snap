08dcc71081f32dd883f05d423d3423af1ba9fac0
Harry Prudden <htprudden@gmail.com>
2021-01-01 13:45:16 +0000

    Initial app layout and card pics added to assets

--- Snap/ViewController.swift
+++ Snap/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardImage:UIImageView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func updateCardImage()
+    {
+        cardImage?.image = UIImage(named: "8H")
+    }
+    
+    @IBAction func drawButtonPressed()
+    {
+        updateCardImage()
+    }
+    
+    @IBAction func snapButtonPressed()
+    {
+        // Check for matching card
+    }
 }
 
